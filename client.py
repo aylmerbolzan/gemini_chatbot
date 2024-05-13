acc370aa14a8bbcc816cccbe0f397347fc1a5439
Aylmer Bolzan <aylmer.bolzan@gmail.com>
2024-05-12 23:36:21 -0300

    feat: criação de client py

---
+++ client.py
@@ -0,0 +1,29 @@
+import google.generativeai as genai
+
+api_key = "AIzaSyD9Ayz4dwW26KcNud9S-PXm7UdZ6QIS3Y4"
+genai.configure(api_key=api_key)
+
+configuracoes = {
+    "candidate_count": 1,
+    "temperature": 0.5,
+}
+
+seguranca = {
+    "HARASSMENT": "BLOCK_NONE",
+    "HATE": "BLOCK_NONE",
+    "SEXUAL": "BLOCK_NONE",
+    "DANGEROUS": "BLOCK_NONE",
+}
+
+model = genai.GenerativeModel(model_name='gemini-1.0-pro',
+                              generation_config=configuracoes,
+                              safety_settings=seguranca)
+
+chat = model.start_chat(history=[])
+
+prompt = input("\n• Comando: \n")
+
+while prompt != "fim":
+    response = chat.send_message(prompt)
+    print("\n• Resposta: ", response.text, "\n")
+    prompt = input("\n• Comando: \n")
